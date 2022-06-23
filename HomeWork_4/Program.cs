03f17f9e7bbfce9358a89ebd4cd426a3b5b9a511
Maksim Sobolev <sobmig@gmail.com>
2022-06-24 00:33:57 +0300

    закончили написание программы (задача 4)

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите первое число");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите третье число");
+int c = Convert.ToInt32 (Console.ReadLine ());
+int max = a;
+
+if (b > max) 
+    {
+    max = b;
+    }
+else
+    if (c > max)
+{
+    max = c;
+}
+ Console.WriteLine (a + ", " + b + ", " + c + " -> " + max);
