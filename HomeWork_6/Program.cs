604e6bcb038e4c6e7afe606fb25890adc9cfa001
Maksim Sobolev <sobmig@gmail.com>
2022-06-24 00:47:15 +0300

    закончили написание программы (задача 6)

---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число");
+double number = Convert.ToDouble (Console.ReadLine ());
+if (number %2 == 0)
+{
+Console.WriteLine (number + " -> " + "да");
+}
+else
+{
+Console.WriteLine (number + " -> " + "нет");
+}
