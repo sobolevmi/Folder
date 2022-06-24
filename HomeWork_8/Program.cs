39b8e8943e373cd3207b964fd05e56c452f6af3e
Maksim Sobolev <sobmig@gmail.com>
2022-06-24 20:41:07 +0300

    закончили написание основной части программы (задача 8, строки 1-20)

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+int i = 1;
+Console.Write (number + " -> ");
+while (i <= (number-2))
+{
+    if (i % 2 == 0)
+    {
+        Console.Write (i + ", ");
+    }
+    i++;
+}
+while (i <= number)
+{
+    if (i % 2 ==0)
+    {
+        Console.Write (i);
+        i++;
+    }
+}
