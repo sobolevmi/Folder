7b5d5478bd6004223fefe6e8529436c39556cb0c
Maksim Sobolev <sobmig@gmail.com>
2022-06-24 20:54:50 +0300

    добавили информацию о программе в строке 1

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine ("Введите первое число");
+﻿// Определение максимального из трех чисел
+Console.WriteLine ("Введите первое число");
 int a = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int b = Convert.ToInt32 (Console.ReadLine());
